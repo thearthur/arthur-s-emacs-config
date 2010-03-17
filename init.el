e15c421f57da9b4776b5f606de0b265b02a60db1
arthur <arthur@cryptovide-dev.(none)>
2010-03-17 00:46:02 -0700

    added my config file

--- init.el
+++ init.el
@@ -1,78 +1,67 @@
-;;; init.el --- Where all the magic begins
-;;
-;; Part of the Emacs Starter Kit
-;;
-;; This is the first thing to get loaded.
-;;
-;; "Emacs outshines all other editing software in approximately the
-;; same way that the noonday sun does the stars. It is not just bigger
-;; and brighter; it simply makes everything else vanish."
-;; -Neal Stephenson, "In the Beginning was the Command Line"
+;; clojure-mode
+(add-to-list 'load-path "~/opt/clojure-mode")
+(require 'clojure-mode)
 
-;; Turn off mouse interface early in startup to avoid momentary display
-;; You really don't need these; trust me.
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;; disable bell function
+(setq ring-bell-function 'ignore)
 
-;; Load path etc.
+;; disable toolbar
+(tool-bar-mode -1)
 
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
+;; disable scrollbar
+(toggle-scroll-bar -1)
 
-(add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/jabber"))
+;; do away with annoying clickey things
+(menu-bar-mode -1)
 
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq package-user-dir (concat dotfiles-dir "elpa"))
-(setq custom-file (concat dotfiles-dir "custom.el"))
+;; disable splash screen
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(show-paren-mode t)
+ '(transient-mark-mode nil))
 
-;; These should be loaded on startup rather than autoloaded on demand
-;; since they are likely to be used in every session
+;; current buffer name in title bar
+(setq frame-title-format "%b")
 
-(require 'cl)
-(require 'saveplace)
-(require 'ffap)
-(require 'uniquify)
-(require 'ansi-color)
-(require 'recentf)
+(column-number-mode)
 
-;; backport some functionality to Emacs 22 if needed
-(require 'dominating-file)
+;; clojure-mode
+(add-to-list 'load-path "~/opt/clojure-mode")
+(require 'clojure-mode)
 
-;; Load up ELPA, the package manager
+;; swank-clojure
+(add-to-list 'load-path "~/opt/swank-clojure/src/emacs")
 
-(require 'package)
-(package-initialize)
-(require 'starter-kit-elpa)
+(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
+      swank-clojure-extra-classpaths (list
+				      "~/opt/swank-clojure/src/main/clojure"
+				      "~/.clojure/clojure-contrib.jar"))
 
-(load "elpa-to-submit/nxhtml/autostart")
+(require 'swank-clojure-autoload)
 
-;; Load up starter kit customizations
+;; slime
+(eval-after-load "slime"
+  '(progn (slime-setup '(slime-repl))))
 
-(require 'starter-kit-defuns)
-(require 'starter-kit-bindings)
-(require 'starter-kit-misc)
-(require 'starter-kit-registers)
-(require 'starter-kit-eshell)
-(require 'starter-kit-lisp)
-(require 'starter-kit-perl)
-(require 'starter-kit-ruby)
-(require 'starter-kit-js)
+(add-to-list 'load-path "~/opt/slime")
+(require 'slime)
+(slime-setup)
 
-(regen-autoloads)
-(load custom-file 'noerror)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
 
-;; You can keep system- or user-specific customizations here
-(setq system-specific-config (concat dotfiles-dir system-name ".el")
-      user-specific-config (concat dotfiles-dir user-login-name ".el")
-      user-specific-dir (concat dotfiles-dir user-login-name))
-(add-to-list 'load-path user-specific-dir)
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+(global-set-key [f11] 'fullscreen)
 
-(if (file-exists-p system-specific-config) (load system-specific-config))
-(if (file-exists-p user-specific-config) (load user-specific-config))
-(if (file-exists-p user-specific-dir)
-  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
-
-;;; init.el ends here
+(set-fringe-style 0)
