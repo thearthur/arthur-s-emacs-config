4f61d8fd02e7a881f3bc3f8c3488b893356c8c79
Arthur Ulfeldt <arthur@ulfeldt.com>
2011-09-21 16:25:50 -0700

    restore rainbow parens for clojure

--- clojure.el
+++ clojure.el
@@ -1,45 +1,14 @@
 
-;; swank-clojure
-;(add-to-list 'load-path "~/opt/swank-clojure/src/emacs")
-
-;(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
-;      swank-clojure-extra-classpaths (list
-;				      "~/opt/swank-clojure/src/main/clojure"
-;				      "~/.clojure/clojure-contrib.jar"))
-
-;(require 'swank-clojure-autoload)
-
-;; slime
-;(eval-after-load "slime"
-;  '(progn (slime-setup '(slime-repl))))
-
-;(add-to-list 'load-path "~/opt/slime")
-;(require 'slime)
-;(slime-setup)
-
-
-;; clojure-mode
 (add-to-list 'load-path "~/.emacs.d/clojure-mode")
 (require 'clojure-mode)
 (require 'clojurescript-mode)
 
-
-;; swank-clojure
-;(add-to-list 'load-path "~/opt/swank-clojure/src/emacs")
-
-;(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
-;      swank-clojure-extra-classpaths (list
-;				      "~/opt/swank-clojure/src/main/clojure"
-;				      "~/.clojure/clojure-contrib.jar"))
-
-;(require 'swank-clojure-autoload)
-
-;; slime
-;(eval-after-load "slime"
-;  '(progn (slime-setup '(slime-repl))))
-
-(add-to-list 'load-path "~/opt/slime")
+(add-to-list 'load-path "~/.emacs.d/slime")
 (require 'slime)
 (slime-setup)
 
 (provide 'clojure)
+
+(load-file "rainbow-delimiters.el")
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
