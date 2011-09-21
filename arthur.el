b78437f37bf3f8dc2bedd26aeabcd491874bd647
Arthur Ulfeldt <arthur@ulfeldt.com>
2011-09-21 14:40:53 -0700

    add back my changes and slime

---
+++ arthur.el
@@ -0,0 +1,17 @@
+;; clojure-mode
+;(add-to-list 'load-path "~/opt/clojure-mode")
+;(require 'clojure-mode)
+
+;; disable bell function
+(setq ring-bell-function 'ignore)
+
+;; disable toolbar
+(tool-bar-mode -1)
+
+;; disable scrollbar
+(toggle-scroll-bar -1)
+
+;; do away with annoying clickey things
+(menu-bar-mode -1)
+
+(provide 'arthur)
