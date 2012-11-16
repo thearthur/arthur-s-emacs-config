b31e961f5a4b427a016727bf7f8ddc93c91b1662
Arthur Ulfeldt <aulfeldt@hytrust.com>
2012-11-15 21:04:28 -0800

    use dropbox for org mode sync

--- init.el
+++ init.el
@@ -1,16 +1,20 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode nrepl org-mode)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode nrepl org org-plus-contrib)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p))) 
 
+(setq org-directory "~/Dropbox/org/")
+(setq org-mobile-directory "~/Dropbox/org/")
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/log.org")
