8a5e5e59cd144070d70e349bff7c0aa53409897b
Arthur Ulfeldt <arthur@yummly.com>
2013-02-08 16:20:48 -0800

    a bunch more clojure hooks

--- init.el
+++ init.el
@@ -8,12 +8,25 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode nrepl org rainbow-delimiters slime-repl)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode nrepl auto-complete ac-nrepl org rainbow-delimiters)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p))) 
+
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+     '(add-to-list 'ac-modes 'nrepl-mode))
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
 (setq org-directory "~/Dropbox/org/")
 (setq org-mobile-directory "~/Dropbox/org/")
