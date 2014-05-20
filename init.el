8d68f3877e1e576ee0237a9fa53562828a7f785b
Arthur Ulfeldt <arthur@yummly.com>
2014-05-19 18:59:11 -0700

    unthread emacs-starter-kit

--- init.el
+++ init.el
@@ -1,14 +1,17 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
+(defvar my-packages '(better-defaults
+		      paredit paredit-everywhere
+                      idle-highlight-mode find-file-in-project smex ido-ubiquitous magit
                       clojure-mode dash cider auto-complete ac-nrepl
                       org rainbow-delimiters auto-complete ace-jump-mode go-mode
                       projectile)
@@ -19,51 +22,75 @@
     (package-install p))) 
 
 (require 'ac-nrepl)
-(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
 
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(eval-after-load "auto-complete"
+  '(progn
+     (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+     (add-to-list 'ac-modes 'cider-mode)
+     (defun set-auto-complete-as-completion-at-point-function ()
+       (setq completion-at-point-functions '(auto-complete)))
+     (add-hook 'prog-mode-hook 'auto-complete-mode)))
+
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+
+(require 'cider)
+(require 'cider-eldoc)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
 (add-hook 'cider-mode-hook 'ac-nrepl-setup)
 (add-hook 'cider-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-mode))
-
+(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (add-hook 'cider-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(require 'cider-eldoc)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'cider-turn-on-eldoc-mode)
 (setq nrepl-hide-special-buffers t)
 (setq cider-auto-select-error-buffer t)
 (setq cider-repl-display-in-current-window nil)
 (setq cider-repl-print-length 100)
+
+
+
+
+(require 'paredit)
+(define-key paredit-mode-map (kbd "C-M-]") 'paredit-forward-barf-sexp)
+(define-key paredit-mode-map (kbd "C-M-[") 'paredit-backward-barf-sexp)
+(define-key paredit-mode-map (kbd "M-]") 'paredit-forward-slurp-sexp)
+(define-key paredit-mode-map (kbd "M-[") 'paredit-backward-slurp-sexp)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-
-(eval-after-load "cider"
-    '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
-
-(add-hook 'prog-mode-hook 'auto-complete-mode)
-
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
-(eval-after-load 'paredit '(define-key paredit-mode-map (kbd "C-M-]") 'paredit-forward-barf-sexp))
-(eval-after-load 'paredit '(define-key paredit-mode-map (kbd "C-M-[") 'paredit-backward-barf-sexp))
-(eval-after-load 'paredit '(define-key paredit-mode-map (kbd "M-]") 'paredit-forward-slurp-sexp))
-(eval-after-load 'paredit '(define-key paredit-mode-map (kbd "M-[") 'paredit-backward-slurp-sexp))
+
+(require 'paredit-everywhere)
+(add-hook 'prog-mode-hook 'paredit-everywhere-mode)
+
+(add-hook 'clojure-mode-hook (lambda () (set-fill-column 0)))
 
 (autoload
   'ace-jump-mode-pop-mark
   "ace-jump-mode"
   "Ace jump back:-)"
   t)
+
 (eval-after-load "ace-jump-mode"
   '(ace-jump-mode-enable-mark-sync))
 (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 (setq x-select-enable-clipboard t)
-(add-hook 'clojure-mode-hook (lambda () (set-fill-column 0)))
+(defun yank-to-x-clipboard ()
+  (interactive)
+  (if (region-active-p)
+        (progn
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          (message "Yanked region to clipboard!")
+          (deactivate-mark))
+    (message "No region active; can't yank to clipboard!")))
+
+(define-key global-map (kbd "M-W") 'yank-to-x-clipboard)
+
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 
